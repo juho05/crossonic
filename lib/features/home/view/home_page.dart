c988b0ed028c4ff78f417ac5ea0e9b69ef91ec7e
Julian Hofmann <git@julianh.de>
2024-06-07 20:00:06 +0200

    Fix pull-to-refresh not working on home page

--- lib/features/home/view/home_page.dart
+++ lib/features/home/view/home_page.dart
@@ -24,32 +24,34 @@ class HomePage extends StatelessWidget {
           create: (_) => RecentlyAddedAlbumsCubit(apiRepository)..fetch(15),
         ),
       ],
-      child: Scaffold(
-        appBar: createAppBar(context, "Home"),
-        body: RefreshIndicator.adaptive(
-          onRefresh: () async {
-            context.read<RecentlyAddedAlbumsCubit>().fetch(15);
-            context.read<RandomSongsCubit>().fetch(50);
-          },
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                HomeCarousel(
-                  title: "Recently added albums",
-                  content: const RecentlyAddedAlbums(),
-                  onMore: () {
-                    context.push("/home/albums/added");
-                  },
-                ),
-                const SizedBox(height: 8),
-                const RandomSongs(),
-              ],
+      child: Builder(builder: (context) {
+        return Scaffold(
+          appBar: createAppBar(context, "Home"),
+          body: RefreshIndicator.adaptive(
+            onRefresh: () async {
+              context.read<RecentlyAddedAlbumsCubit>().fetch(15);
+              context.read<RandomSongsCubit>().fetch(50);
+            },
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  HomeCarousel(
+                    title: "Recently added albums",
+                    content: const RecentlyAddedAlbums(),
+                    onMore: () {
+                      context.push("/home/albums/added");
+                    },
+                  ),
+                  const SizedBox(height: 8),
+                  const RandomSongs(),
+                ],
+              ),
             ),
           ),
-        ),
-      ),
+        );
+      }),
     );
   }
 }
