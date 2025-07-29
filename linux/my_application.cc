a4d2829eac509a3ce098991b3c6143747d2d482d
Julian Hofmann <git@julianh.de>
2025-07-29 15:55:28 +0200

    Improve AppImage compatibility

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -131,6 +131,7 @@ static void my_application_init(MyApplication *self) {}
 
 MyApplication *my_application_new()
 {
+  g_set_prgname(APPLICATION_ID);
   return MY_APPLICATION(g_object_new(my_application_get_type(),
                                      "application-id", APPLICATION_ID,
                                      "flags", G_APPLICATION_NON_UNIQUE,
