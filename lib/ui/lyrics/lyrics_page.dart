b92ea9528f0108d2867f2ff5d3785a85621192b2
Julian Hofmann <git@julianh.de>
2025-10-27 18:36:16 +0100

    Improve synced lyrics page
    - unify synced and unsynced lyrics
    - scroll synced lyrics
    - seek by tapping on lyrics line
    - use custom widget instead of flutter_lyric

--- lib/ui/lyrics/lyrics_page.dart
+++ lib/ui/lyrics/lyrics_page.dart
@@ -1,11 +1,14 @@
+import 'dart:async';
+
 import 'package:auto_route/auto_route.dart';
+import 'package:crossonic/data/repositories/subsonic/models/lyrics.dart';
 import 'package:crossonic/ui/common/buttons.dart';
 import 'package:crossonic/ui/lyrics/lyrics_viewmodel.dart';
 import 'package:crossonic/utils/fetch_status.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_lyric/lyrics_reader.dart';
-import 'package:flutter_lyric/lyrics_reader_model.dart';
+import 'package:flutter/rendering.dart';
 import 'package:provider/provider.dart';
+import 'package:super_sliver_list/super_sliver_list.dart';
 
 @RoutePage()
 class LyricsPage extends StatefulWidget {
@@ -18,6 +21,14 @@ class LyricsPage extends StatefulWidget {
 class _LyricsPageState extends State<LyricsPage> {
   late final LyricsViewModel _viewModel;
 
+  // 0.0 (top) - 1.0 (bottom) current line position
+  static const double _bias = 0.3;
+
+  final _scrollController = ScrollController();
+  final _listController = ListController();
+
+  StreamSubscription? _selectedLineSub;
+
   @override
   void initState() {
     super.initState();
@@ -25,142 +36,189 @@ class _LyricsPageState extends State<LyricsPage> {
       subsonic: context.read(),
       audioHandler: context.read(),
     );
+
+    Lyrics? previousLyrics;
+    _selectedLineSub = _viewModel.selectedLine.listen((event) {
+      if (previousLyrics != null && previousLyrics != _viewModel.lyrics) {
+        _scrollController.jumpTo(0);
+        previousLyrics = _viewModel.lyrics;
+      }
+      _animateToCurrentLine();
+    });
+  }
+
+  void _animateToCurrentLine() {
+    Future.delayed(const Duration(milliseconds: 50), () {
+      WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+        if (_viewModel.syncedMode) {
+          if (_viewModel.selectedLine.value == null) {
+            _scrollController.animateTo(
+              0,
+              duration: const Duration(milliseconds: 300),
+              curve: Curves.easeInOut,
+            );
+          } else {
+            _listController.animateToItem(
+              index: _viewModel.selectedLine.value!,
+              alignment: _bias,
+              scrollController: _scrollController,
+              duration: (estimatedDistance) =>
+                  const Duration(milliseconds: 300),
+              curve: (estimatedDistance) => Curves.easeInOut,
+            );
+          }
+        }
+      });
+    });
   }
 
   @override
   void dispose() {
+    _selectedLineSub?.cancel();
     _viewModel.dispose();
+    _listController.dispose();
+    _scrollController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("Lyrics")),
-      body: LayoutBuilder(
-        builder: (context, constraints) {
-          return ListenableBuilder(
-            listenable: _viewModel,
-            builder: (context, _) {
-              if (_viewModel.status == FetchStatus.failure) {
-                return const Center(child: Icon(Icons.wifi_off));
-              }
-              if (_viewModel.status != FetchStatus.success) {
-                return const Center(
-                  child: CircularProgressIndicator.adaptive(),
-                );
-              }
-              if (_viewModel.currentSong == null) {
-                return const Center(child: Text("No active song"));
-              }
-              if (_viewModel.lyrics == null) {
-                return const Center(child: Text("No lyrics"));
-              }
-              final lyricsModel = LyricsReaderModel();
-              lyricsModel.lyrics = _viewModel.lyrics!.lines.map((e) {
-                final model = LyricsLineModel();
-                model.mainText = e.text;
-                model.startTime = e.start?.inMilliseconds;
-                model.endTime =
-                    e.end?.inMilliseconds ??
-                    _viewModel.currentSong?.duration?.inMilliseconds;
-                return model;
-              }).toList();
-              if (!_viewModel.syncedMode) {
-                return Padding(
-                  padding: const EdgeInsets.only(
-                    left: 8,
-                    right: 8,
-                    bottom: 32,
-                    top: 8,
+    return ListenableBuilder(
+      listenable: _viewModel,
+      builder: (context, _) {
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text("Lyrics"),
+            forceMaterialTransparency: true,
+            actions: [
+              if (_viewModel.supportsSync && !_viewModel.syncedMode)
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 12),
+                  child: Button(
+                    onPressed: () => _viewModel.syncedMode = true,
+                    icon: Icons.access_time,
+                    outlined: true,
+                    child: const Text("Sync"),
                   ),
-                  child: ListView.builder(
-                    itemCount:
-                        _viewModel.lyrics!.lines.length +
-                        (_viewModel.supportsSync ? 1 : 0),
-                    itemBuilder: (context, index) {
-                      if (_viewModel.supportsSync) {
-                        if (index == 0) {
-                          return Align(
-                            alignment: Alignment.centerRight,
-                            child: Button(
-                              outlined: !_viewModel.syncedMode,
-                              onPressed: () => _viewModel.syncedMode =
-                                  !_viewModel.syncedMode,
-                              child: Text(
-                                _viewModel.syncedMode ? "Synced" : "Unsynced",
-                              ),
-                            ),
-                          );
-                        }
-                        index--;
-                      }
-                      return Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 8),
-                        child: Text(
-                          _viewModel.lyrics!.lines[index].text,
-                          textAlign: TextAlign.center,
-                          softWrap: true,
-                          style: Theme.of(context).textTheme.headlineMedium!
-                              .copyWith(
-                                fontSize: constraints.maxWidth >= 1000
-                                    ? 25
-                                    : 18,
-                              ),
-                        ),
-                      );
-                    },
+                )
+              else if (_viewModel.lyrics != null)
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 12),
+                  child: Text(
+                    _viewModel.syncedMode ? "SYNCED" : "UNSYNCED",
+                    style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                      fontWeight: FontWeight.w300,
+                    ),
                   ),
-                );
-              }
-              return Padding(
-                padding: const EdgeInsets.only(left: 8, right: 8, top: 8),
-                child: Column(
-                  children: [
-                    Align(
-                      alignment: Alignment.centerRight,
-                      child: Button(
-                        outlined: !_viewModel.syncedMode,
-                        onPressed: () =>
-                            _viewModel.syncedMode = !_viewModel.syncedMode,
-                        child: Text(
-                          _viewModel.syncedMode ? "Synced" : "Unsynced",
-                        ),
-                      ),
-                    ),
-                    Expanded(
-                      child: StreamBuilder(
-                        stream: _viewModel.position,
-                        builder: (context, _) {
-                          return IgnorePointer(
-                            // TODO custom lyrics implementation that allows scrolling and clicking on line to seek
-                            child: LyricsReader(
-                              model: lyricsModel,
-                              lyricUi: UINetease(
-                                highlight: false,
-                                bias: 0.35,
-                                defaultSize: constraints.maxWidth >= 1000
-                                    ? 35
-                                    : 25,
-                                otherMainSize: constraints.maxWidth >= 1000
-                                    ? 25
-                                    : 18,
-                              ),
-                              playing: _viewModel.playing,
-                              position:
-                                  _viewModel.position.value.inMilliseconds,
-                            ),
-                          );
-                        },
-                      ),
-                    ),
-                  ],
                 ),
-              );
-            },
-          );
-        },
-      ),
+            ],
+          ),
+          body: SafeArea(
+            child: LayoutBuilder(
+              builder: (context, constraints) {
+                if (_viewModel.status == FetchStatus.failure) {
+                  return const Center(child: Icon(Icons.wifi_off));
+                }
+                if (_viewModel.status != FetchStatus.success) {
+                  return const Center(
+                    child: CircularProgressIndicator.adaptive(),
+                  );
+                }
+                if (_viewModel.currentSong == null) {
+                  return const Center(child: Text("No active song"));
+                }
+                if (_viewModel.lyrics == null ||
+                    _viewModel.lyrics!.lines.isEmpty) {
+                  return const Center(child: Text("No lyrics"));
+                }
+                final textTheme = Theme.of(context).textTheme.headlineMedium!;
+                final normalTextTheme = textTheme.copyWith(
+                  fontSize: constraints.maxWidth >= 1000 ? 24 : 18,
+                );
+                final highlightedTextTheme = textTheme.copyWith(
+                  fontSize: constraints.maxWidth >= 1000 ? 28 : 22,
+                  fontWeight: FontWeight.bold,
+                );
+                return NotificationListener<UserScrollNotification>(
+                  onNotification: (notification) {
+                    if (notification.direction == ScrollDirection.idle) {
+                      return false;
+                    }
+                    _viewModel.syncedMode = false;
+                    return true;
+                  },
+                  child: CustomScrollView(
+                    controller: _scrollController,
+                    slivers: [
+                      SliverPadding(
+                        padding: const EdgeInsets.all(8),
+                        sliver: SuperSliverList.builder(
+                          listController: _listController,
+                          itemCount: _viewModel.lyrics!.lines.length,
+                          itemBuilder: (context, index) {
+                            final line = _viewModel.lyrics!.lines[index];
+                            return StreamBuilder(
+                              stream: _viewModel.selectedLine.distinct(
+                                (previous, next) =>
+                                    (previous == index) == (next == index),
+                              ),
+                              builder: (context, snapshot) {
+                                final isCurrent =
+                                    _viewModel.syncedMode &&
+                                    snapshot.data == index;
+                                return Padding(
+                                  padding: EdgeInsets.symmetric(
+                                    horizontal: !isCurrent ? 8 : 0,
+                                  ),
+                                  child: Align(
+                                    alignment: Alignment.center,
+                                    child: ClipRRect(
+                                      borderRadius: BorderRadius.circular(8),
+                                      child: Material(
+                                        child: InkWell(
+                                          onTap:
+                                              line.start != null &&
+                                                  _viewModel.supportsSync
+                                              ? () {
+                                                  _viewModel.seek(line.start!);
+                                                }
+                                              : null,
+                                          child: Padding(
+                                            padding: EdgeInsets.symmetric(
+                                              vertical: 8.0,
+                                              horizontal: line.text.isNotEmpty
+                                                  ? 12
+                                                  : 50,
+                                            ),
+                                            child: Text(
+                                              line.text.isNotEmpty
+                                                  ? line.text
+                                                  : "♪♫♪",
+                                              textAlign: TextAlign.center,
+                                              softWrap: true,
+                                              style: isCurrent
+                                                  ? highlightedTextTheme
+                                                  : normalTextTheme,
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                );
+                              },
+                            );
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+        );
+      },
     );
   }
 }
--- lib/ui/lyrics/lyrics_viewmodel.dart
+++ lib/ui/lyrics/lyrics_viewmodel.dart
@@ -19,11 +19,14 @@ class LyricsViewModel extends ChangeNotifier {
 
   bool get supportsSync => _lyrics?.synced ?? false;
 
-  bool _syncedMode = false;
+  bool _syncedMode = true;
   bool get syncedMode => _syncedMode && supportsSync;
 
   set syncedMode(bool sync) {
+    if (syncedMode == sync) return;
     _syncedMode = sync;
+    _onPositionChanged(force: true);
+    _updatePositionTimer();
     _updateWakelock();
     notifyListeners();
   }
@@ -37,13 +40,8 @@ class LyricsViewModel extends ChangeNotifier {
   Lyrics? _lyrics;
   Lyrics? get lyrics => _lyrics;
 
-  final BehaviorSubject<Duration> _position = BehaviorSubject.seeded(
-    Duration.zero,
-  );
-  ValueStream<Duration> get position => _position.stream;
-
-  bool get playing =>
-      _audioHandler.playbackStatus.value == PlaybackStatus.playing;
+  final BehaviorSubject<int?> _selectedLine = BehaviorSubject.seeded(null);
+  ValueStream<int?> get selectedLine => _selectedLine.stream;
 
   LyricsViewModel({
     required SubsonicRepository subsonic,
@@ -59,6 +57,7 @@ class LyricsViewModel extends ChangeNotifier {
 
   Future<void> _fetch() async {
     _lyrics = null;
+    _onPositionChanged();
     if (_currentSong == null) {
       _status = FetchStatus.success;
       notifyListeners();
@@ -76,31 +75,106 @@ class LyricsViewModel extends ChangeNotifier {
     }
     _status = FetchStatus.success;
     _lyrics = result.value;
+    _syncedMode = supportsSync;
+    _updatePositionTimer();
     _updateWakelock();
     notifyListeners();
   }
 
   Timer? _positionTimer;
   Future<void> _onStatusChanged(PlaybackStatus status) async {
-    notifyListeners();
-    if (status == PlaybackStatus.playing) {
-      _positionTimer ??= Timer.periodic(
-        const Duration(milliseconds: 250),
-        (_) => _position.add(_audioHandler.position),
-      );
+    _updatePositionTimer();
+  }
+
+  void _updatePositionTimer() {
+    _onPositionChanged();
+    if (_audioHandler.playbackStatus.value == PlaybackStatus.playing &&
+        syncedMode) {
+      _startPositionTimer();
     } else {
-      _positionTimer?.cancel();
-      _positionTimer = null;
+      _stopPositionTimer();
+    }
+  }
+
+  void _startPositionTimer() {
+    if (_positionTimer != null) return;
+    _positionTimer = Timer.periodic(
+      const Duration(milliseconds: 100),
+      (_) => _onPositionChanged(),
+    );
+  }
+
+  void _stopPositionTimer() {
+    bool wasOn = _positionTimer != null;
+    _positionTimer?.cancel();
+    _positionTimer = null;
+    if (wasOn) {
+      _onPositionChanged();
+    }
+  }
+
+  void _onPositionChanged({bool force = false}) {
+    if (lyrics == null) {
+      if (_selectedLine.value != null || force) {
+        _selectedLine.add(null);
+      }
+      return;
+    }
+
+    final pos = _audioHandler.position;
+    final currentIndex = _selectedLine.value;
+    final currentLine = currentIndex != null
+        ? lyrics?.lines.elementAtOrNull(_selectedLine.value!)
+        : null;
+    if (!force &&
+        currentLine != null &&
+        currentLine.start != null &&
+        currentLine.start! <= pos) {
+      if ((currentLine.end != null && currentLine.end! > pos) ||
+          currentIndex! == lyrics!.lines.length - 1) {
+        // current line is still correct
+        return;
+      }
+      _calculateCurrentLine(pos, currentIndex);
+      return;
+    }
+    _calculateCurrentLine(pos, 0, force: force);
+  }
+
+  void _calculateCurrentLine(
+    Duration pos,
+    int startIndex, {
+    bool force = false,
+  }) {
+    if (lyrics == null) {
+      if (_selectedLine.value != null || force) {
+        _selectedLine.add(null);
+      }
+      return;
+    }
+
+    int? found;
+    for (int i = startIndex; i < lyrics!.lines.length; i++) {
+      final line = lyrics!.lines[i];
+      if (line.start == null) continue;
+      if (line.start! > pos) break;
+      if ((line.end != null && line.end! > pos) ||
+          i == lyrics!.lines.length - 1) {
+        found = i;
+        break;
+      }
+    }
+    if (_selectedLine.value != found || force) {
+      _selectedLine.add(found);
     }
   }
 
   @override
   void dispose() {
-    _updateWakelock(enable: false);
-    _positionTimer?.cancel();
-    _positionTimer = null;
     _currentSubscription?.cancel();
     _statusSubscription?.cancel();
+    _stopPositionTimer();
+    _updateWakelock(enable: false);
     super.dispose();
   }
 
@@ -116,5 +190,14 @@ class LyricsViewModel extends ChangeNotifier {
       );
     }
     await WakelockPlus.toggle(enable: enable);
+  }
+
+  Future<void> seek(Duration duration) async {
+    final paused = _audioHandler.playbackStatus.value == PlaybackStatus.paused;
+    await _audioHandler.seek(duration);
+    if (paused) {
+      await _audioHandler.play();
+    }
+    syncedMode = true;
   }
 }
