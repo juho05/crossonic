5b18ac07546b530cd31c95d255c171b5704c7fdd
Julian Hofmann <git@julianh.de>
2025-12-21 13:09:05 +0100

    Improve year picker

--- lib/ui/years/year_selector_field.dart
+++ lib/ui/years/year_selector_field.dart
@@ -1,5 +1,8 @@
-import 'package:crossonic/ui/common/dialogs/year_picker.dart';
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:month_picker_dialog/month_picker_dialog.dart';
 
 class YearSelectorField extends StatelessWidget {
   final int year;
@@ -9,12 +12,13 @@ class YearSelectorField extends StatelessWidget {
 
   final void Function(int year) onChanged;
 
-  const YearSelectorField(
-      {super.key,
-      required this.year,
-      required this.onChanged,
-      this.minYear,
-      this.maxYear});
+  const YearSelectorField({
+    super.key,
+    required this.year,
+    required this.onChanged,
+    this.minYear,
+    this.maxYear,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -24,10 +28,24 @@ class YearSelectorField extends StatelessWidget {
         color: Theme.of(context).colorScheme.surfaceContainer,
         child: InkWell(
           onTap: () async {
-            final year = await YearPickerDialog.show(context,
-                minYear: minYear, maxYear: maxYear);
-            if (year == null) return;
-            onChanged(year);
+            final y = await showYearPicker(
+              context: context,
+              initialDate: DateTime(year),
+              firstDate: minYear != null ? DateTime(minYear!) : null,
+              lastDate: maxYear != null ? DateTime(maxYear!) : null,
+              monthPickerDialogSettings: MonthPickerDialogSettings(
+                dialogSettings: PickerDialogSettings(
+                  blockScrolling:
+                      kIsWeb ||
+                      Platform.isWindows ||
+                      Platform.isMacOS ||
+                      Platform.isLinux,
+                  dismissible: true,
+                ),
+              ),
+            );
+            if (y == null) return;
+            onChanged(y);
           },
           child: Padding(
             padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
